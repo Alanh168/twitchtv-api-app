33d486571feed86ee07bf886d3e94785e2c23478
Alan Hernandez <alan.m.hernandez@gmail.com>
2017-11-17 00:21:09 -0500

    Added heading and search bar for app

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
-jQuery(document).ready(function($) {
+$(document).ready(function() {
+	function fetchResults() {
+		const endpoint = ``;
+		fetch(endpoint)
+		.then(response => response.json())
+		.then(data)
+		displayResults();
+	}
+
+	function displayResults() {
+
+	}
+
+	/*
+	USAGE:
+Replace the Twitch API base URL https://api.twitch.tv/kraken with https://wind-bow.gomix.me/twitch-api. Use this endpoint according to the Twitch API documentation.
+
+NOTE:
+This server caches data to lower the request rate. To prevent abuses this server accepts GET requests only, and serves only routes /users/:user, /channels/:channel, and /streams/:stream. These are 
+
+	*/
 });
